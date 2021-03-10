2b1448573bde6e8db4dbca257e1ffe60e596736d
vblank182 <33797815+vblank182@users.noreply.github.com>
2021-03-10 18:25:55 -0500

    Testing

--- blaseball_stlats_tlracker/load_db.py
+++ blaseball_stlats_tlracker/load_db.py
@@ -8,7 +8,24 @@ def saveData():
 
     ## TEST
     import redis, os
-    redis_url = os.getenv("REDIS_URL").encode("idna")  # encode properly for redis package
+
+    print("________")
+    print(os.getenv("REDIS_URL"))
+    print("--------")
+    print(os.getenv("REDIS_TLS_URL"))
+    print("--------")
+    print(os.getenv("PATH"))
+    print("^^^^^^^^")
+
+    print("________")
+    print(os.getenvb("REDIS_URL"))
+    print("--------")
+    print(os.getenvb("REDIS_TLS_URL"))
+    print("--------")
+    print(os.getenvb("PATH"))
+    print("^^^^^^^^")
+
+    redis_url = os.getenv("REDIS_URL") #.encode("idna")  # encode properly for redis package
     rd = redis.Redis(redis_url)
 
     s = rd.set('foo', 'bar')
