9554d43e4fd15be4a158ffc0127ac83319424ff1
vblank182 <33797815+vblank182@users.noreply.github.com>
2021-03-09 23:27:14 -0500

    Update build_webpage.py

--- blaseball_stlats_tlracker/build_webpage.py
+++ blaseball_stlats_tlracker/build_webpage.py
@@ -51,11 +51,8 @@ stats_list = bst.requestPlayerStatsFromAPI(plIDs, fields)
 abs_dir = os.path.join(folder, "web", "results.html")
 generatePage(abs_dir, [stats_list[0][0], stats_list[0][2], stats_list[0][1]["batting_average"]])
 
-print('results------------')
-
-
 test_dir = os.path.join(folder, "web", "index.html")
-    with open(test_dir, "w") as f:
-        f.write("<html><body>Test Results Page</body></html>")
+with open(test_dir, "w") as f:
+    f.write("<html><body>Test Results Page</body></html>")
 
 #############
