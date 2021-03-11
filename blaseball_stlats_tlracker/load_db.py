891f1eaded4a70e1d9cd500ed17bcc6257d3cffd
vblank182 <33797815+vblank182@users.noreply.github.com>
2021-03-10 20:10:16 -0500

    Update load_db.py

--- blaseball_stlats_tlracker/load_db.py
+++ blaseball_stlats_tlracker/load_db.py
@@ -2,12 +2,16 @@
 # Jesse Williams 🎸
 # Requires Python >= 3.9
 
+# Redis endpoint anatomy:
+#  [[scheme]]             [[userinfo user:pass (with user field blank)]]                                [[host]]                      [[port]]
+#    redis  ://  :p9xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxe7  @  ec0-00-000-000-000.compute-1.amazonaws.com  :  25xx9
+
 def saveData():
     # Data:
     # {fullName, location, team_nickname, team_emoji, }
 
     ## TEST
-    import redis, os
+    import redis, os, urllib
 
     print("________")
     print(os.getenv("REDIS_URL"))
@@ -17,17 +21,16 @@ def saveData():
     print(os.getenv("PATH"))
     print("^^^^^^^^")
 
-    print("________")
-    print(os.getenvb("REDIS_URL"))
-    print("--------")
-    print(os.getenvb("REDIS_TLS_URL"))
-    print("--------")
-    print(os.getenvb("PATH"))
-    print("^^^^^^^^")
+    # Get Redis location from Heroku env variable and parse URI
+    rd_url = os.getenv("REDIS_URL")
+    rd_pw, rd_host = urllib.parse.urlparse(rd_url).netloc.split("@")
+    rd_port = urllib.parse.urlparse(rd_url).port
 
-    redis_url = os.getenv("REDIS_URL") #.encode("idna")  # encode properly for redis package
-    rd = redis.Redis(redis_url)
+    print(f'{rd_url}  ==  {rd_host} : {rd_port} with {rd_pw}')
 
+    rd = redis.Redis(host=rd_host, port=rd_port, password=rd_pw)
+
+    print("~~~~~~~~")
     s = rd.set('foo', 'bar')
     print(s)
     g = rd.get('foo')
