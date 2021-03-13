c8522840daaaa833033052615677a054c442e7d6
vblank182 <33797815+vblank182@users.noreply.github.com>
2021-03-13 02:59:28 -0500

    Test

--- web/app.py
+++ web/app.py
@@ -11,12 +11,15 @@ bst_frontend = Flask(__name__)
 @bst_frontend.route("/")
 def index():
 
-    names = ["Aldon Cashmoney", "York Silk", "Goodwin Morin", "Wyatt Glover", "Ren Hunter"]
+    names = ["Goodwin Morin", "Aldon Cashmoney", "York Silk", "Wyatt Glover", "Ren Hunter"]
 
     players = getPlayerStatsByName(names, 'batter')
 
-    # Render HTML with count variable
-    return render_template("index.html", data={'name': playerDatas[0], 'team': f'{playerData[1]} {playerData[2]}', 'batting_average': playerData[14]})
+    ## TEST Render HTML
+    return render_template(
+        "index.html",
+        player=players[0]
+    )
 
 
 if __name__ == "__main__":
