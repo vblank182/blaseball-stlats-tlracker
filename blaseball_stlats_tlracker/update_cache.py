04f594bd5b850c3579dbab7be9bed09519544661
vblank182 <33797815+vblank182@users.noreply.github.com>
2021-03-14 23:06:34 -0400

    Test (and error checking)

--- blaseball_stlats_tlracker/update_cache.py
+++ blaseball_stlats_tlracker/update_cache.py
@@ -2,21 +2,29 @@
 # Jesse Williams 🎸
 
 from blaseball_stlats_tlracker import updatePlayerStatCache
+import os
 
-# Read list of batter names from file
-with open('../common/players_batters.txt') as f:
-    batterNames = f.readlines()
-    batterNames = [name.strip() for name in batterNames]
+try:
+    print(os.getcwd())
+    # Read list of batter names from file
+    with open('../common/players_batters.txt') as f:
+        batterNames = f.readlines()
+        batterNames = [name.strip() for name in batterNames]
 
-# Read list of pitcher names from file
-with open('../common/players_pitchers.txt') as f:
-    pitcherNames = f.readlines()
-    pitcherNames = [name.strip() for name in pitcherNames]
+    # Read list of pitcher names from file
+    with open('../common/players_pitchers.txt') as f:
+        pitcherNames = f.readlines()
+        pitcherNames = [name.strip() for name in pitcherNames]
 
-# Update batters
-if len(batterNames) > 0:
-    updatePlayerStatCache(batterNames, 'batter')
+    # Update batters
+    if len(batterNames) > 0:
+        updatePlayerStatCache(batterNames, 'batter')
 
-# Update pitchers
-if len(pitcherNames) > 0:
-    updatePlayerStatCache(pitcherNames, 'pitcher')
+    # Update pitchers
+    if len(pitcherNames) > 0:
+        updatePlayerStatCache(pitcherNames, 'pitcher')
+
+    print('[Debug] Successfully updated player stat cache.')
+
+except:
+    print('[Error] Could not update player stat cache.')
